fcf90b5fe34c5c3c2b45e60d35ca145609bb1a89
SerVi8 <sergiovargasumacacho@gmail.com>
2025-06-13 22:33:52 -0400

    implementacionde tailwindcss y shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import path from "node:path";
 import electron from "vite-plugin-electron/simple";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,13 +25,19 @@ export default defineConfig({
       renderer:
         process.env.NODE_ENV === "test"
           ? // https://github.com/electron-vite/vite-plugin-electron-renderer/issues/78#issuecomment-2053600808
-            undefined
+          undefined
           : {},
     }),
+    tailwindcss(),
   ],
   server: {
     proxy: {
       "/api": "http://localhost:8080",
     },
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 });

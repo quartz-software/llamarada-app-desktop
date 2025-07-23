22c8184a7dadd56ce0e65e956c18f61e2189571e
SerVi8 <sergiovargasumacacho@gmail.com>
2025-07-22 23:08:26 -0400

    Actualizacion de la pagina de tarifas

--- src/pages/RoomRate/RoomRates.tsx
+++ src/pages/RoomRate/RoomRates.tsx
@@ -1,25 +1,16 @@
-import Input from "../../shared/components/Input";
-import FormField from "../../shared/components/FormField";
-import Button from "../../shared/components/Button";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPen } from "@fortawesome/free-solid-svg-icons";
 import "./RoomRates.css";
 import { Tarifa } from "@/shared/types/db/tarifa";
-
-const formatDate = (dateData?: Date) => {
-  if (!dateData) return "";
-  const isoDate = new Date(dateData).toISOString()
-  let date = isoDate.split("T")[0];
-  let dateParts = date.split("-");
-
-  return `${dateParts[2]}/${dateParts[1]}/${dateParts[0]}`;
-};
+import RoomRateList from "./components/RoomRateList";
+import { Label } from "@/shared/components/ui/label";
+import { Input } from "@/shared/components/ui/input";
+import { Button } from "@/shared/components/ui/button";
 
 const RoomRates = () => {
   const nav = useNavigate();
   const [roomRatesData, setRoomRatesData] = useState<Tarifa[]>([]);
+  const [searchText, setSearchText] = useState("")
 
   async function getData() {
     try {
@@ -36,65 +27,27 @@ const RoomRates = () => {
     getData();
   }, []);
 
+  const filteredRates = roomRatesData.filter((rate) =>
+    rate.precio.toString().includes(searchText)
+  )
+
   return (
-    <div>
-      <h1>Tarifas de habitacion</h1>
-      <div className="div--search">
-        <FormField label="Buscar" errorMessage="">
+    <div className="space-y-5">
+      <h1 className="font-bold text-2xl">Tarifas de habitación</h1>
+      <div className="flex justify-between items-end">
+        <div>
+          <Label className="mb-2 block">Buscar por precio</Label>
           <Input
-            handleInput={() => { }}
-            resetMessage={() => { }}
-            placeholder="Buscar"
             type="text"
-            value=""
+            placeholder="Buscar..."
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            className="w-[200px]"
           />
-        </FormField>
-        <Button
-          disabled={false}
-          handleClick={() => {
-            nav(`/rates/edit`);
-          }}
-        >
-          Agregar
-        </Button>
+        </div>
+        <Button onClick={() => nav("/rates/edit")}>Agregar</Button>
       </div>
-      <table className="table--rates">
-        <thead>
-          <th>Id</th>
-          <th>Fecha Inicio</th>
-          <th>Fecha Fin</th>
-          <th>Precio/noche</th>
-          <th>Estado</th>
-          <th></th>
-        </thead>
-        {roomRatesData.length == 0 ? (
-          <div className="div--nd">No se encomtraron cuartos</div>
-        ) : (
-          <tbody>
-            {roomRatesData.map((tarifa: Tarifa, index) => {
-              return (
-                <tr key={index}>
-                  <td>{tarifa.id}</td>
-                  <td>{formatDate(tarifa.fechaInicio)}</td>
-                  <td>{formatDate(tarifa.fechaFin)}</td>
-                  <td>{tarifa.precio}</td>
-                  <td>{tarifa.activo ? "Activo" : "Deshabilitado"}</td>
-                  <td>
-                    <Button
-                      disabled={false}
-                      handleClick={() => {
-                        nav(`/rates/edit?id=${tarifa.id}`);
-                      }}
-                    >
-                      <FontAwesomeIcon icon={faPen} />
-                    </Button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        )}
-      </table>
+      <RoomRateList rates={filteredRates} />
     </div>
   );
 };
---
+++ src/pages/RoomRate/components/RoomRateList.tsx
@@ -0,0 +1,71 @@
+import { Button } from "@/shared/components/ui/button"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/shared/components/ui/table"
+import { Tarifa } from "@/shared/types/db/tarifa"
+import { ArrowUpRightFromSquareIcon } from "lucide-react"
+import { FC } from "react"
+import { useNavigate } from "react-router-dom"
+
+interface RoomRateListProps {
+  rates: Tarifa[]
+}
+
+const formatDate = (dateData?: Date) => {
+  if (!dateData) return "";
+  const isoDate = new Date(dateData).toISOString()
+  let date = isoDate.split("T")[0];
+  let dateParts = date.split("-");
+
+  return `${dateParts[2]}/${dateParts[1]}/${dateParts[0]}`;
+};
+
+const RoomRateList: FC<RoomRateListProps> = ({ rates }) => {
+  const nav = useNavigate();
+
+  return (
+    <div className="overflow-hidden rounded-lg border">
+      <Table>
+        <TableHeader className="bg-muted">
+          <TableRow>
+            <TableHead>Id</TableHead>
+            <TableHead>Fecha Inicio</TableHead>
+            <TableHead>Fecha Fin</TableHead>
+            <TableHead>Precio/noche</TableHead>
+            <TableHead>Estado</TableHead>
+            <TableHead className="text-center">Editar</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {rates.length !== 0 ? (
+            rates.map((tarifa, index) => (
+              <TableRow key={index}>
+                <TableCell>{tarifa.id}</TableCell>
+                <TableCell>{formatDate(tarifa.fechaInicio)}</TableCell>
+                <TableCell>{formatDate(tarifa.fechaFin)}</TableCell>
+                <TableCell>{tarifa.precio}</TableCell>
+                <TableCell>{tarifa.activo ? "Activo" : "Deshabilitado"}</TableCell>
+                <TableCell className="text-center">
+                  <Button
+                    variant="outline"
+                    type="button"
+                    className="h-8 w-8 p-0"
+                    onClick={() => nav(`/rates/edit?id=${tarifa.id}`)}
+                  >
+                    <ArrowUpRightFromSquareIcon />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={6} className="h-24 text-center text-muted-foreground">
+                No se encontraron tarifas.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  )
+}
+
+export default RoomRateList
